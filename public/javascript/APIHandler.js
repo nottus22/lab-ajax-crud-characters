a708192077bd1bd7d39566cf67a858ef40c9babe
nottus <mcelroy.sc@gmail.com>
2021-04-10 07:27:59 -0700

    iteration 2

--- public/javascript/APIHandler.js
+++ public/javascript/APIHandler.js
@@ -4,22 +4,22 @@ class APIHandler {
   }
 
   getFullList () {
-
+    return axios.get(this.BASE_URL + '/characters');
   }
 
-  getOneRegister () {
-
+  getOneRegister (id) {
+    return axios.get(this.BASE_URL + `/characters/${id}`)
   }
 
-  createOneRegister () {
-
+  createOneRegister (createChar) {
+    return axios.post(this.BASE_URL + '/characters', createChar)
   }
 
-  updateOneRegister () {
-
+  updateOneRegister (id, updateChar) {
+    return axios.patch(this.BASE_URL + `/characters/${id}`, updateChar)
   }
 
-  deleteOneRegister () {
-
+  deleteOneRegister (id) {
+    return axios.delete(this.BASE_URL + `/characters/${id}`)
   }
 }
